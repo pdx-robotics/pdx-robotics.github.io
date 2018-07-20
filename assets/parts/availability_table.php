88dd00b7b461f38d5c9b0b77fdd07ceb70daca27
Aaron Chan <chanaar@pdx.edu>
2018-07-20 16:13:55 -0700

    Fix: Actual working PHP version.

---
+++ assets/parts/availability_table.php
@@ -0,0 +1,32 @@
+<!-- Weekly Availability Links Table -->
+<div class="table-wrapper">
+    <table>
+        <thead>
+            <h4 class="align-center">Weekly Availability Poll</h4>
+            <tr>
+                <th>School Year</th>
+                <th>Fall (Oct - Dec)</th>
+                <th>Winter (Jan - Mar)</th>
+                <th>Spring (April - June)</th>
+                <th>Summer (June - Sep)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <!-- School Year 2017-2018 -->
+                <td>2017-2018</td>
+                <td></td>
+                <td><s>Winter 2018</s></td>
+                <td><s>Spring 2018</s></td>
+                <td><a href="https://doodle.com/poll/iehuw42gf9s4hme9">Summer 2018</a></td>
+            </tr>
+            <tr>
+                <!-- School Year 2018-2019 -->
+                <td>2018-2019</td>
+                <td>Fall 2019</td>
+                <td></td>
+                <td></td>
+                <td></td>
+        </tbody>
+    </table>
+</div>
---
+++ get_involved.php
@@ -0,0 +1,3 @@
+<?php
+require "./assets/get_involved.php";
+?>
