a64bbcb5cff008f37ba0591c04cf541ac35ec1ae
robotics account <robotics@rita.cecs.pdx.edu>
2019-06-19 16:29:43 -0700

    added links to the inventory and the checkout form

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
 <body>
+<h2>Admin Section</h2>
+<a href="/admin/inventory">inventory</a><br>
+<a href="/admin/checkout.php">check out form and database</a><br>
 <?php
-echo "php script";
 ?>
 </body>
 </html>
