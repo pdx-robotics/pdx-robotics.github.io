de68baed1bdd9c6e3a39063fecf73d9fbe7e175e
Geoffrey Olson <geoolson@pdx.edu>
2019-06-20 13:58:29 -0700

    added insert and remove events to the events.php
    for admins

--- admin/events.php
+++ admin/events.php
@@ -2,6 +2,25 @@
 <html>
 <body>
 <h2>Events</h2>
+
+<!-- form for adding a new event -->
+<form method="POST">
+  <h2>Append to events</h2>
+  Name:<br>
+  <input type="text" name="name"><br>
+  Term:<br>
+  <input type="text" name="term"><br>
+  Year:<br>
+  <input type="text" name="year"><br>
+  Day:<br>
+  <input type="text" name="day"><br>
+  Time:<br>
+  <input type="text" name="time"><br>
+  location/room:<br>
+  <input type="text" name="room"><br>
+  <input type="submit" value="Submit">
+</form>
+
 <?php
 $servername = "db.cecs.pdx.edu";
 $username = "geoolson";
@@ -22,6 +41,41 @@ function test_input($data) {
   return $data;
 }
 
+// handling POST requests
+$name = $year = $day = $time = $room = $term = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+  // deleting items from the inventory
+  if ( $_POST["delete"]) {
+    $keys = array_keys($_POST);
+    foreach($keys as $entry) {
+      $sql = "DELETE FROM events WHERE id=\"" . $entry . "\"";
+      $result = mysqli_query($conn, $sql);
+    }
+  }
+  
+  // adding an event to events
+  else{
+    $name = test_input($_POST["id"]);
+    $name = test_input($_POST["name"]);
+    $year = test_input($_POST["year"]);
+    $day = test_input($_POST["day"]);
+    $time = test_input($_POST["time"]);
+    $room = test_input($_POST["room"]);
+    $term = test_input($_POST["term"]);
+    $sql = "INSERT INTO events (id, name, year, day, time, room, term) 
+    VALUE(UUID(),
+    \"" . $name . "\"," . $year . ",\"" .$day . "\",\"" .$time . "\",\"" .$room. "\",\"" .$term. "\")";
+    try{
+      $result = mysqli_query($conn, $sql);
+    }
+    catch (Exception $e) {
+      echo $e->getMessage();
+    }
+  }
+}
+
+// Table of events
 $sql = "SELECT * FROM events";
 $result = mysqli_query($conn, $sql);
 if(mysqli_num_rows($result) > 0){
@@ -33,6 +87,7 @@ if(mysqli_num_rows($result) > 0){
       <th><input type='submit' value='Delete'></th>
       <th>term</th>
       <th>year</th>
+      <th>name</th>
       <th>day</th>
       <th>time</th>
       <th>room</th>
@@ -45,12 +100,14 @@ if(mysqli_num_rows($result) > 0){
     "<tr><td><input type ='checkbox' name='". $row["id"]. "'/></td><td>".
     $row["term"] . "</td><td>" . 
     $row["year"]. "</td><td>" . 
+    $row["name"]. "</td><td>" . 
     $row["day"]. "</td><td>" . 
     $row["time"]. "</td><td>". 
     $row["room"]. "</td></tr>";
   }
   echo "</form></table>";
 }
+// end of table of events
 ?>
 </body>
 </html>
