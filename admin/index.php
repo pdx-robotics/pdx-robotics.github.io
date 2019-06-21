4a1c789e90ea06894d0efde3c9accade4c9a32cb
Geoffrey Olson <geoolson@pdx.edu>
2019-06-21 10:35:01 -0700

    Image and links are in two separate column nows

--- admin/index.php
+++ admin/index.php
@@ -15,14 +15,20 @@
 include "../parts/header.html";
 ?>
 <div class="inner">
-<header class="special">
-<h2>Admin Section</h2>
-<img src="responsibility.jpg" alt="With great power comes great responsibility"><br>
-<a href="/admin/inventory">inventory</a><br>
-<a href="/admin/checkout.php">check out form and database</a><br>
-<a href="/admin/events.php">events on front page</a><br>
-<a href="/admin/gallery.php">gallery</a><br>
-</header>
+  <h2>Admin Section</h2>
+  <div class="row">
+    <div class="col-4 col-12-medium">
+      <ul style="list-style-type:none;">
+      <li><a href="/admin/inventory">inventory</a></li>
+      <li><a href="/admin/checkout.php">check out form and database</a></li>
+      <li><a href="/admin/events.php">events on front page</a></li>
+      <li><a href="/admin/gallery.php">gallery</a></li>
+      </ul>
+    </div>
+    <div class="col-4 col-12-medium">
+      <img src="responsibility.jpg" alt="With great power comes great responsibility">
+    </div>
+  </div>
 </div>
 
 <!-- Scripts -->
