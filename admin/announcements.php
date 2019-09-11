2c5c546de02022714031346e91ba7f57b32ad004
Geoffrey Olson <geoolson@pdx.edu>
2019-09-10 18:47:34 -0700

    removed first template for announcements

--- admin/announcements.php
+++ admin/announcements.php
@@ -6,31 +6,6 @@
     include '../db.php';
     $sql = "SELECT * FROM announcements";
     $result = mysqli_query($conn, $sql);?>
-    <section class='wrapper'>
-        <div class='inner'>
-            <h2>Announcements</h2>
-            <table>
-    <?php
-    foreach($result as $row){
-        echo '<tr><td>'.PHP_EOL;
-        if(isset($row['attachment'])){
-            echo '<div class="row">';
-            echo '<div class="col-7 col-12-medium">'.PHP_EOL;
-        }
-        echo $parsedown->text($row['announcement']);
-        if(isset($row['attachment']) && $row['attachment'] != ""){
-            echo '</div>'.PHP_EOL;
-            echo '<div class="col-5 col-12-medium">'.PHP_EOL;
-            echo '<img src="';
-            echo $row['attachment'];
-            echo '" style="max-width: 300px;">'.PHP_EOL;
-            echo '</div></div>'.PHP_EOL;
-        }
-        echo '</td></tr>'.PHP_EOL;
-    }?>
-            </table>
-        </div>
-    </section>
     <section id="main" class="wrapper">
         <div class="inner">
             <div class="content">
