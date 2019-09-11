f086bb54617b0731d55f77dd8f85a48c7646b66f
Geoffrey Olson <geoolson@pdx.edu>
2019-09-10 18:11:08 -0700

    announcements api and a template in admin section
    also removed pictures that were part of the original template for
    the site that aren't used.

--- admin/announcements.php
+++ admin/announcements.php
@@ -12,11 +12,50 @@
             <table>
     <?php
     foreach($result as $row){
-        echo '<tr><td>';
+        echo '<tr><td>'.PHP_EOL;
+        if(isset($row['attachment'])){
+            echo '<div class="row">';
+            echo '<div class="col-7 col-12-medium">'.PHP_EOL;
+        }
         echo $parsedown->text($row['announcement']);
+        if(isset($row['attachment']) && $row['attachment'] != ""){
+            echo '</div>'.PHP_EOL;
+            echo '<div class="col-5 col-12-medium">'.PHP_EOL;
+            echo '<img src="';
+            echo $row['attachment'];
+            echo '" style="max-width: 300px;">'.PHP_EOL;
+            echo '</div></div>'.PHP_EOL;
+        }
         echo '</td></tr>'.PHP_EOL;
     }?>
             </table>
+        </div>
+    </section>
+    <section id="main" class="wrapper">
+        <div class="inner">
+            <div class="content">
+                <header>
+                    <h2>announcements</h2>
+                </header>
+                <?php
+                foreach($result as $row){
+                    if(isset($row['attachment']) && $row['attachment'] != ""){
+                        echo '<div class="row">';
+                        echo '<div class="col-7 col-12-medium">'.PHP_EOL;
+                    }
+                    echo $parsedown->text($row['announcement']).PHP_EOL;
+                    if(isset($row['attachment']) && $row['attachment'] != ""){
+                        echo '</div>';
+                        echo '<div class="col-5 col-12-medium"><img src="';
+                        echo $row['attachment'];
+                        echo '" style="max-width: 300px;"></p>'.PHP_EOL;
+                        echo '</div>';
+                        echo '</div>';
+                    }
+                    echo '<hr>'.PHP_EOL;
+                }
+                ?>
+            </div>
         </div>
     </section>
     <?php include "../parts/footer.html";?>
--- api/announcements.php
+++ api/announcements.php
@@ -3,19 +3,27 @@ include '../db.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 $data = $_POST['data'];
+$attachment = $_POST['attachment'];
 $username = $conn->real_escape_string($username);
 $password = $conn->real_escape_string($password);
 $data = $conn->real_escape_string($data);
+$attachment = $conn->real_escape_string($attachment);
 
 $sql = "SELECT admin FROM users WHERE username='$username' AND password=PASSWORD('$password')";
 $result = mysqli_query($conn, $sql);
 if(mysqli_num_rows($result) > 0){
   $row = mysqli_fetch_assoc($result);
   if($row['admin'] == 2){
-    echo "he be an admin";
     $_SESSION['admin'] = True;
     $_SESSION['credentialCorrect'] = True;
-    $sql = "INSERT INTO announcements(announcement) VALUES('$data')";
+    if(isset($_POST['attachment'])){
+      $sql = "INSERT INTO announcements(announcement, attachment) VALUES('$data', '$attachment')";
+      echo "recieved attachment and text";
+    }
+    else{
+      $sql = "INSERT INTO announcements(announcement) VALUES('$data')";
+      echo "recieved a text announcement";
+    }
     $result = mysqli_query($conn, $sql);
   }
   else{
@@ -24,9 +32,9 @@ if(mysqli_num_rows($result) > 0){
   }
 }
 else{
+    echo "failure; recieved the following from your request:".PHP_EOL;
     echo $_POST['username'].PHP_EOL;
     echo $_POST['password'].PHP_EOL;
     echo $_POST['data'].PHP_EOL;
-    echo array_keys($_POST);
 }
 ?>
