64042484739aafea3a729f871fd452517e3759ca
Geoffrey Olson <geoolson@pdx.edu>
2019-12-30 21:40:56 -0800

    switched to using pdo and prepared statements

--- api/announcements.php
+++ api/announcements.php
@@ -1,40 +1,35 @@
 <?php
-include '../db.php';
+include "../dbpdo.php";
 $username = $_POST['username'];
 $password = $_POST['password'];
 $data = $_POST['data'];
 $attachment = $_POST['attachment'];
-$username = $conn->real_escape_string($username);
-$password = $conn->real_escape_string($password);
-$data = $conn->real_escape_string($data);
-$attachment = $conn->real_escape_string($attachment);
 
-$sql = "SELECT admin FROM users WHERE username='$username' AND password=PASSWORD('$password')";
-$result = mysqli_query($conn, $sql);
-if(mysqli_num_rows($result) > 0){
-  $row = mysqli_fetch_assoc($result);
-  if($row['admin'] == 2){
-    $_SESSION['admin'] = True;
-    $_SESSION['credentialCorrect'] = True;
+$stmt = $pdo->prepare("SELECT admin FROM users WHERE username=? AND password=PASSWORD(?)");
+$stmt->execute([$username, $password]);
+$result = $stmt->fetchAll();
+if(sizeof($result) == 1){
+  $result = $result[0];
+  if($result['admin'] == 2){
     if(isset($_POST['attachment'])){
-      $sql = "INSERT INTO announcements(announcement, attachment) VALUES('$data', '$attachment')";
+      $stmt = $pdo->prepare("INSERT INTO announcements(announcement, attachment) VALUES(?, ?)");
+      $stmt->execute([$data, $attachment]);
       echo "recieved attachment and text";
     }
     else{
-      $sql = "INSERT INTO announcements(announcement) VALUES('$data')";
+      $stmt = $pdo->prepare("INSERT INTO announcements(announcement) VALUES(?)");
+      $stmt->execute([$data]);
       echo "recieved a text announcement";
     }
-    $result = mysqli_query($conn, $sql);
   }
   else{
-    $_SESSION['admin'] = False;
     echo "You do not have administrative privileges";
   }
 }
 else{
-    echo "failure; recieved the following from your request:".PHP_EOL;
-    echo $_POST['username'].PHP_EOL;
-    echo $_POST['password'].PHP_EOL;
-    echo $_POST['data'].PHP_EOL;
+  echo "failure; recieved the following from your request:".PHP_EOL;
+  echo $_POST['username'].PHP_EOL;
+  echo $_POST['password'].PHP_EOL;
+  echo $_POST['data'].PHP_EOL;
 }
 ?>
